c1aec975dcc44efaf62e6697db08b746e85ff9c6
Isaquelopes91 <isaque.lopes047@gmail.com>
2022-09-04 23:00:18 -0300

    add

--- public/JS/api/getFiltroSetor.js
+++ public/JS/api/getFiltroSetor.js
@@ -30,32 +30,27 @@ busca.addEventListener('focusout', () => {
                 var tdId = document.createElement('td');
                 var tdTecnico = document.createElement('td');
                 var tdSetor = document.createElement('td');
-                var tdTipoAtendimento = document.createElement('td');
-                var tdTclassificacaoDeAssistencia = document.createElement('td');
                 var tdmotivoDaAssistencia = document.createElement('td');
                 var tdreclamacaoCliente = document.createElement('td');
-                var tdobservacao = document.createElement('td');
                 var tddataEHora = document.createElement('td');
+                var buttonDelete = document.createElement('a')
 
                 tdId.textContent = ultimos[i].order;
                 tdTecnico.textContent = ultimos[i].nomeTecnico;
                 tdSetor.textContent = ultimos[i].setorAtendimeneto;
-                tdTipoAtendimento.textContent = ultimos[i].tipoDeAtendimento;
-                tdTclassificacaoDeAssistencia.textContent = ultimos[i].classificacaoDeAssistencia;
                 tdmotivoDaAssistencia.textContent = ultimos[i].motivoDaAssistencia;
                 tdreclamacaoCliente.textContent = ultimos[i].reclamacaoCliente;
-                tdobservacao.textContent = ultimos[i].observacao;
                 tddataEHora.textContent = ultimos[i].dataEHora;
+                buttonDelete.innerHTML = `<img src="../icones/info.png" alt="">`
+                buttonDelete.setAttribute("href", `./infoPage.html?order=${ultimos[i].order}`);
 
                 tr.appendChild(tdId);
                 tr.appendChild(tdTecnico);
                 tr.appendChild(tdSetor);
-                tr.appendChild(tdTipoAtendimento);
-                tr.appendChild(tdTclassificacaoDeAssistencia);
                 tr.appendChild(tdmotivoDaAssistencia);
                 tr.appendChild(tdreclamacaoCliente);
-                tr.appendChild(tdobservacao);
                 tr.appendChild(tddataEHora);
+                tr.appendChild(buttonDelete);
                 corpoTabela.appendChild(tr);
 
                 let informa = document.querySelector('.encontradoRegistros')
